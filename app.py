f60bfe8180217bc537c0b89afb815de7166f1bc8
Turhan Coşkun <turhancoskun@gmail.com>
2015-08-22 07:23:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Flask is running!(changed from github)'
+    return 'Flask is running!(github)'
 
 if __name__ == '__main__':
     app.run()
