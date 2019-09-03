1e2b8e658e1d0f37be7e394bc51c5eaf96f4bcac
Jha <ashwajha@8c8590433595.ant.amazon.com>
2019-09-03 17:22:32 +0530

    added . instead of ./

--- scripts/restart_supervisor.sh
+++ scripts/restart_supervisor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./$PWD/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deploy.sh
+.$PWD/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deploy.sh
 echo "This is a test. I am going to use a long running process deploy.sh here."
-./$PWD/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/sleep.sh
+.$PWD/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/sleep.sh
 
