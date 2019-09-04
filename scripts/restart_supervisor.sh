778744f8ed651228f5cdc836c22cc980d538ddeb
root <root@ip-172-31-27-98.ec2.internal>
2019-09-04 10:56:26 +0000

    Sent stderr and stdout to /dev/null

--- scripts/restart_supervisor.sh
+++ scripts/restart_supervisor.sh
@@ -6,6 +6,6 @@ sudo yum install python-setuptools
 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/deploy.sh
 echo "This is a test. I am going to use a long running process deploy.sh here."
 #flask run /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/app.py
-/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/sleep.sh > /dev/null 2 > /dev/null < dev/null &
+/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/sleep.sh &> /dev/null &
 
 
