f485e8804ee8bcca135b2848e0952d513aa5e9b5
root <root@ip-172-31-27-98.ec2.internal>
2019-09-04 07:02:19 +0000

    Added pip.

--- scripts/restart_supervisor.sh
+++ scripts/restart_supervisor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo yum install -y python-pip
+sudo yum install python-setuptools
+
 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/deploy.sh
 echo "This is a test. I am going to use a long running process deploy.sh here."
 /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/scripts/sleep.sh
